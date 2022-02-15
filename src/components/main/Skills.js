2c01b74cdf5900b169697e519c505146ace3c6f4
Jawad Erfani <jawad.erfani147@gmail.com>
2022-02-15 16:18:54 +0430

    Technologies page has been responsived

--- src/components/main/Skills.js
+++ src/components/main/Skills.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function Skills() {
   return (
-    <div id="skills" className="px-28 m-4 text-gray-200 dark:text-slate-700">
-      <h1 className="text-6xl py-6 font-semibold">Technologies</h1>
-      <p className="font-semibold text-2xl pb-6">
+    <div id="skills" className="xl:px-28  px-8 my-6  text-gray-300 dark:text-slate-700">
+      <h1 className="xl:text-7xl text-4xl sm:text-5xl md:text-6xl  py-6 font-semibold">Technologies</h1>
+      <p className="font-serif md:font-semibold xl:text-2xl md:text-xl text-lg pb-6">
         My skills are not confined only to MERN stack but I've worked with
         multiple web technologies
       </p>
       <div className="py-8 shadow-lg">
-        <ul className="flex  justify-between items-center">
+        <ul className="flex flex-wrap justify-between items-center">
           <li className="text-center mx-10 ">
             <img src="logo512.png" alt="react" className="w-12 h-12 my-2" />
             <span>React</span>
