25e6c04ebb0004d391145a46a0182e1e61284524
Jawad Erfani <jawad.erfani147@gmail.com>
2022-04-25 05:18:29 +0430

    new projects added

--- src/components/PUblicData/Data.js
+++ src/components/PUblicData/Data.js
@@ -1,24 +1,40 @@
 export const projects = [
   {
+    image: "https://i.nextmedia.com.au/News/change_your_region_to_the_us.png",
+    name: "Country app",
+    des: "This is a Country app that you can see list of all country and you can see much more information about every county that you want",
+    tegUse: ["React", "Context", "Typescript", "tailwindCss"],
+    gitUrl:'https://github.com/JawadErfani01/Country-app',
+    liveUrl:'https://countryapp01.netlify.app/',
+   
+  },
+  {
     image:
       "https://www.smarther.co/wp-content/uploads/2020/07/mobile-ecommerce-blog.jpg",
     name: "Maxpot Ecommerce",
     des: "This is a frontend Ecommerce Store for Mixed Martial Art Productswhere you create your account and use it for checkout process",
     tegUse: ["React", "context", "Typescript", "tailwindCss"],
-    overviesButton: ["Code", "Live"],
+    gitUrl:'https://github.com/JawadErfani01/E-CommerceApp',
+    liveUrl:'https://ecommerceapp01.netlify.app/',
+   
   },
   {
-    image: "https://i.nextmedia.com.au/News/change_your_region_to_the_us.png",
+    image: "screenshot.jpg",
     name: "Todo List",
     des: "This is a full Todo list you can add update and remove a todo in your list and also every actions has a good alert message",
     tegUse: ["React", "Javascript", "css"],
-    overviesButton: ["Code", "Live"],
+    gitUrl:'https://github.com/JawadErfani01/todolist',
+    liveUrl:'https://todolist0001.netlify.app/',
+   
   },
+
   {
-    image: "https://i.nextmedia.com.au/News/change_your_region_to_the_us.png",
-    name: "Country app",
-    des: "This is a Country app that you can see list of all country and you can see much more information about every county that you want",
-    tegUse: ["React", "Context", "Typescript", "tailwindCss"],
-    overviesButton: ["Code", "Live"],
+    image: "reduxtodo.png",
+    name: "Redux todoList",
+    des: "This is a full Todo list you can add update and remove a todo in your list and also every actions has a good alert message",
+    tegUse: ["React","Redux", "Javascript", "css"],
+    gitUrl:'https://github.com/JawadErfani01/Redux-todolist',
+    liveUrl:'https://reduxtodolist01.netlify.app/',
+   
   },
 ];
--- src/components/main/Projects.js
+++ src/components/main/Projects.js
@@ -10,12 +10,12 @@ function Projects() {
       <div className="md:flex flex-wrap justify-center items-center mx-6 sm:mx-0  text-gray-300">
     
           {projects.map((project,index) => {
-            const { image, name, des, tegUse, overviesButton } = project;
+            const { image, name, des, tegUse, gitUrl,liveUrl } = project;
             return (
               <section className="md:w-3/5 lg:w-2/5  bg-gradient-to-l dark:via-blue-300 dark:bg-slate-300 dark:from-blue-300  dark:text-gray-700  from-slate-800 via-cyan-900  sm:w-4/6 w-full mx-auto text-center shadow-xl shadow-slate-500 rounded-2xl bg-slate-900 py-7 p-4  my-8  md:my-16 md:mx-4 lg:mx-10">
                 <img
                   src={image}
-                  className=" mx-auto items-center  lg:max-h-56 rounded-t-xl mb-4 hover:scale-105 duration-500 ease-in-out"
+                  className=" mx-auto items-center w-full h-uto lg:h-56 rounded-t-xl mb-4 hover:scale-105 duration-500 ease-in-out"
                   alt={name}
                 />
                 <h2 className="text-2xl md:text-3xl font-normal">{name}</h2>
@@ -35,15 +35,26 @@ function Projects() {
                 })}
                 </div>
                 <div className="flex justify-center items-center  ">
-                {overviesButton.map((btn,index) => {
-                  return (
-                    <div key={index} className="mx-auto px-4 ">
-                      <button className="px-4 py-2 dark:via-blue-300 dark:bg-slate-300 dark:from-blue-400  dark:text-gray-700 rounded-lg bg-blue-500 shadow-md bg-gradient-to-l from-slate-800 via-cyan-900  hover:bg-blue-600 text-white shadow-cyan-600">
-                        {btn}
+               
+                  
+                    <div className="mx-auto px-4 ">
+                      <button>
+                       <a   
+                        className="px-4 py-2.5 dark:via-blue-300 dark:bg-slate-300 dark:from-blue-400  dark:text-gray-700 rounded-lg bg-blue-500 shadow-md bg-gradient-to-l from-slate-800 via-cyan-900  hover:bg-blue-600 text-white shadow-cyan-600"
+                       target="_blank" href={gitUrl}>Code</a> 
                       </button>
+                   
                     </div>
-                  );
-                })}
+                  
+                    <div className="mx-auto px-4 ">
+                      <button >
+                       <a
+                       className="px-4 py-2.5 dark:via-blue-300 dark:bg-slate-300 dark:from-blue-400  dark:text-gray-700 rounded-lg bg-blue-500 shadow-md bg-gradient-to-l from-slate-800 via-cyan-900  hover:bg-blue-600 text-white shadow-cyan-600"
+                       target="_blank" href={liveUrl}>Live</a> 
+                      </button>
+                   
+                    </div>
+          
                 </div>
                 </section>
             );
