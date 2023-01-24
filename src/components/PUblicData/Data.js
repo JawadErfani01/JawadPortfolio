419cbbbd8a4b7a5435ece58bbd6ac1a4d60f75d0
OmarsSedigh <omarssedig@gmail.com>
2023-01-25 00:16:04 +0430

    E-Commerse project link updated

--- src/components/PUblicData/Data.js
+++ src/components/PUblicData/Data.js
@@ -1,40 +1,37 @@
 export const projects = [
   {
+    image:
+      "https://www.smarther.co/wp-content/uploads/2020/07/mobile-ecommerce-blog.jpg",
+    name: "Nazho Store",
+    des: "This is a frontend Ecommerce Store for Tailwind Css Art Productswhere you create your account and use it for checkout process",
+    tegUse: ["React", "Redux", "tailwindCss"],
+    gitUrl: "https://github.com/JawadErfani01/E-CommerceApp",
+    liveUrl: "https://ecommerceapp01.netlify.app/",
+  },
+  {
     image: "https://i.nextmedia.com.au/News/change_your_region_to_the_us.png",
     name: "Country app",
     des: "This is a Country app that you can see list of all country and you can see much more information about every county that you want",
     tegUse: ["React", "Context", "Typescript", "tailwindCss"],
-    gitUrl:'https://github.com/JawadErfani01/Country-app',
-    liveUrl:'https://countryapp01.netlify.app/',
-   
+    gitUrl: "https://github.com/JawadErfani01/Country-app",
+    liveUrl: "https://countryapp01.netlify.app/",
   },
-  {
-    image:
-      "https://www.smarther.co/wp-content/uploads/2020/07/mobile-ecommerce-blog.jpg",
-    name: "Maxpot Ecommerce",
-    des: "This is a frontend Ecommerce Store for Mixed Martial Art Productswhere you create your account and use it for checkout process",
-    tegUse: ["React", "context", "Typescript", "tailwindCss"],
-    gitUrl:'https://github.com/JawadErfani01/E-CommerceApp',
-    liveUrl:'https://ecommerceapp01.netlify.app/',
-   
-  },
+
   {
     image: "screenshot.jpg",
     name: "Todo List",
     des: "This is a full Todo list you can add update and remove a todo in your list and also every actions has a good alert message",
     tegUse: ["React", "Javascript", "css"],
-    gitUrl:'https://github.com/JawadErfani01/todolist',
-    liveUrl:'https://todolist0001.netlify.app/',
-   
+    gitUrl: "https://github.com/JawadErfani01/todolist",
+    liveUrl: "https://todolist0001.netlify.app/",
   },
 
   {
     image: "reduxtodo.png",
     name: "Redux todoList",
     des: "This is a full Todo list you can add update and remove a todo in your list and also every actions has a good alert message",
-    tegUse: ["React","Redux", "Javascript", "css"],
-    gitUrl:'https://github.com/JawadErfani01/Redux-todolist',
-    liveUrl:'https://reduxtodolist01.netlify.app/',
-   
+    tegUse: ["React", "Redux", "Javascript", "css"],
+    gitUrl: "https://github.com/JawadErfani01/Redux-todolist",
+    liveUrl: "https://reduxtodolist01.netlify.app/",
   },
 ];
